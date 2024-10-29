457407d12785be5d26c84ad4b9e94ae7e2907d16
owiltonlucas <wiltonlucas.owl@gmail.com>
2024-10-28 23:47:22 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './components/pages/Home';
+import Contact from './components/pages/Contact';
+import Company from './components/pages/Company';
+import NewProject from './components/pages/NewProject';
+
+import Container from './components/layout/Container'; // Corrigido aqui
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/contact">Contato</Link>
+        <Link to="/company">Empresa</Link>
+        <Link to="/newproject">Novo Projeto</Link>
+      </div>
+
+      <Container customClass="min-height">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/company" element={<Company />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/newproject" element={<NewProject />} />
+        </Routes>
+      </Container>
+
+      <p>Footer</p>
+    </Router>
+  );
+}
+
+export default App;
