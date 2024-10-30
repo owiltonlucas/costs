eb0eb614af9d5469040684f6bb6a193cf3af8a70
owiltonlucas <wiltonlucas.owl@gmail.com>
2024-10-30 09:01:13 -0300

    commit_2

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,16 @@ import Home from './components/pages/Home';
 import Contact from './components/pages/Contact';
 import Company from './components/pages/Company';
 import NewProject from './components/pages/NewProject';
+import Projects from './components/pages/Projects';
 
-import Container from './components/layout/Container'; // Corrigido aqui
+import Container from './components/layout/Container'; 
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
 
 function App() {
   return (
     <Router>
-      <div>
-        <Link to="/">Home</Link>
-        <Link to="/contact">Contato</Link>
-        <Link to="/company">Empresa</Link>
-        <Link to="/newproject">Novo Projeto</Link>
-      </div>
+     <Navbar />
 
       <Container customClass="min-height">
         <Routes>
@@ -22,10 +20,11 @@ function App() {
           <Route path="/company" element={<Company />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/newproject" element={<NewProject />} />
+          <Route path="/projects" element={<Projects />} />
         </Routes>
       </Container>
 
-      <p>Footer</p>
+      <Footer />
     </Router>
   );
 }
