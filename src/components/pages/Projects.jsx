9d53cbe86d4bf34edac85c95860e961a286d3731
owiltonlucas <wiltonlucas.owl@gmail.com>
2024-11-01 14:26:11 -0300

    commit_6

--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -1,5 +1,29 @@
+import { useLocation } from 'react-router-dom';
+import Message from '../layout/Message'
+import Container from '../layout/Container'
+import LinkButton from '../layout/LinkBotton'
+import styles from './Projects.module.css'
+
 function Projects() {
-    return <h1>Projects</h1>
+
+    const location = useLocation()
+    let message = ''
+    if(location.state) {
+        message = location.state.message
+    }
+
+    return (
+        <div className={styles.project_container}>
+            <div className={styles.title_container}>
+                <h1>Meus Projetos</h1>
+                <LinkButton to="/newproject" text="Criar Projeto"/>
+            </div>
+            {message && <Message type="success" msg={message} />}
+            <Container customClass="start">
+                <p>Projetos...</p>
+            </Container>
+        </div>
+    )
 }
 
 export default Projects;
