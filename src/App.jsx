930fccf53ad9bb0676a1cc33e9ddc23c910814e7
owiltonlucas <wiltonlucas.owl@gmail.com>
2024-11-02 00:50:54 -0300

    commit_9

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Contact from './components/pages/Contact';
 import Company from './components/pages/Company';
 import NewProject from './components/pages/NewProject';
 import Projects from './components/pages/Projects';
+import Project from './components/pages/Project'
 
 import Container from './components/layout/Container'; 
 import Navbar from './components/layout/Navbar';
@@ -21,6 +22,7 @@ function App() {
           <Route path="/contact" element={<Contact />} />
           <Route path="/newproject" element={<NewProject />} />
           <Route path="/projects" element={<Projects />} />
+          <Route path="/project/:id" element={<Project />} />
         </Routes>
       </Container>
 
